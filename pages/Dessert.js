0b3c62d596465396ec48b8c79d30cc546cc77802
Marc Becker <maarc1@gmx.de>
2022-10-11 20:54:47 +0200

    link in config added, img still not working

--- pages/Dessert.js
+++ pages/Dessert.js
@@ -1,3 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const imageDessert = "https://images.unsplash.com/photo-1603236268617-d023914d9416?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+
 export default function Dessert() {
-  return <>This is the Dessert-Page</>;
+  return (
+    <>
+      <h2>This is the Dessert-Page</h2>
+      <Image
+        src={imageDessert}
+        alt="Picture of a bad pizza"
+        width={500}
+        height={500}
+        layout="responsive"
+      />
+    </>
+  );
 }
