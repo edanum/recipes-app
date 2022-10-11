380bfe61809a65e26749fe20cf9bc3908d3f9f0b
Marc Becker <maarc1@gmx.de>
2022-10-11 16:35:11 +0200

    part3: image from public directory

--- pages/Veggie.js
+++ pages/Veggie.js
@@ -1,3 +1,16 @@
+import pizza from ".././public/pizza.jpeg";
+
 export default function Veggie() {
-  return <>This is the Veggie-Page</>;
+  return (
+    <>
+      <h2>This is the Veggie-Page</h2>
+      <Image
+        src={pizza}
+        alt="Picture of a bad pizza"
+        width={500}
+        height={500}
+        layout="responsive"
+      />
+    </>
+  );
 }
